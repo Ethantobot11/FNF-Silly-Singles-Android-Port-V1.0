0c4b54fa3e4e8d55dbee825625fa0ab2eda4fcf8
Ethantobot <ethantobot7@gmail.com>
2026-05-10 12:48:39 +0200

    new changes

--- setup/unix.sh
+++ setup/unix.sh
@@ -13,9 +13,19 @@ haxelib install flixel 5.2.2 --quiet
 haxelib install flixel-addons 2.11.0 --quiet
 haxelib install flixel-ui 2.4.0 --quiet
 haxelib install hscript 2.4.0 --quiet
-haxelib install hxvlc 1.9.0 --quiet
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet
 haxelib git hxcpp https://github.com/PsychExtendedThings/hxcpp --quiet
 haxelib git lime https://github.com/ArkoseLabsOfficial/lime --quiet
 haxelib install openfl 9.2.2 --quiet
+echo Setting dependencies...
+haxelib dev linc_luajit /root/haxelib/linc_luajit/git
+haxelib dev tjson /root/haxelib/tjson/1,4,0
+haxelib dev flixel /root/haxelib/flixel/5,2,2
+haxelib dev flixel-addons /root/haxelib/flixel-addons/2,11,0
+haxelib dev flixel-ui /root/haxelib/flixel-ui/2,4,0
+haxelib dev hscript /root/haxelib/hscript/2,4,0
+haxelib dev hxCodec /root/haxelib/hxCodec/git
+haxelib dev hxcpp /root/haxelib/hxcpp/git
+haxelib dev lime /root/haxelib/lime/git
+haxelib dev openfl /root/haxelib/openfl/9,3,3
 echo Finished!
