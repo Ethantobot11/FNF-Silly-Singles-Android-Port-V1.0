c403aa7b826c66260e8852643ffa105add987396
Ethantobot <ethantobot7@gmail.com>
2026-06-09 00:16:25 +0200

    Replace sys.io.File with funk.PsychFile and FileSystem

--- source/editors/DialogueCharacterEditorState.hx
+++ source/editors/DialogueCharacterEditorState.hx
@@ -30,7 +30,8 @@ import flixel.group.FlxSpriteGroup;
 import lime.system.Clipboard;
 import Alphabet;
 #if sys
-import sys.io.File;
+import funk.PsychFile as File;
+import funk.PsychFileSystem as FileSystem;;
 #end
 
 using StringTools;
